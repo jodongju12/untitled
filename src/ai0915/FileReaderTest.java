0bde8861dcc663331193fe652192b0c725e2f76f
jodongju12 <2601110302@gspace.kopo.ac.kr>
2026-09-15 16:25:59 +0900

    11장 파일 입출력(전체 파일 내용 반대로 출력)

---
+++ src/ai0915/FileReaderTest.java
@@ -0,0 +1,36 @@
+package ai0915;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderTest {
+
+    public static void main(String[] args) {
+
+        try {
+            // File을 읽어오기 위한 입력스트림(InputStream) 생성
+            BufferedReader br = new BufferedReader(new FileReader("D:/FileIOTest/myData1.txt"));
+
+            String line = "";
+
+            // File에서 한 줄 읽어오기
+        while (true) {
+            line = br.readLine();
+            if (line == null)
+                break;
+            System.out.println(line);
+        }
+
+            // FileInputStream 닫기
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/ai0915/FileReaderTest3.java
@@ -0,0 +1,42 @@
+package ai0915;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderTest3 {
+//      파일의 내용 전체를 거꾸로 출력
+
+    public static void main(String[] args) {
+
+        try {
+            StringBuilder sb = new StringBuilder();
+            // File을 읽어오기 위한 입력스트림(InputStream) 생성
+        //    BufferedReader br = new BufferedReader(new FileReader("D:/FileIOTest/myData1.txt"));//절대경로
+            BufferedReader br = new BufferedReader(new FileReader("myData1.txt"));//상대경로
+
+            String line = "";
+
+            // File에서 한 줄 읽어오기
+        while (true) {
+            line = br.readLine();
+            if (line == null)
+                break;
+            sb.append(line).append("\n");
+        }
+
+            // FileInputStream 닫기
+            br.close();
+
+            sb.reverse();
+            System.out.println(sb);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
